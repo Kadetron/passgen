e3d61bbbd68dda767cc1a46460897687e90e9a2b
Amitabh <121345266+Kadetron@users.noreply.github.com>
2023-02-09 03:20:04 +0530

    Update passgen.sh

--- passgen.sh
+++ passgen.sh
@@ -1,26 +1,26 @@
-﻿#!/bin/bash
+#!/bin/bash
 count=0
- read -p "Enter the length of password: " n
+read -p "Enter the length of password: " n
 integer() {
-        i=( {48..57} )
-        echo -n ${i[$[ $RANDOM % 10 ]]} | awk '{printf("%c",$1)}'
+    i=( {48..57} )
+    echo -n ${i[$[ $RANDOM % 10 ]]} | awk '{printf("%c",$1)}'
 }
 smletter() {
-        s=( {97..122} )
-        echo -n ${s[$[ $RANDOM % 26 ]]} | awk '{printf("%c",$1)}'
+    s=( {97..122} )
+    echo -n ${s[$[ $RANDOM % 26 ]]} | awk '{printf("%c",$1)}'
 }
 spchar() {
-        sp=( {32..47} {58..64} )
-        echo -n ${sp[$[ $RANDOM % 23 ]]} | awk '{printf("%c",$1)}'
+    sp=( {32..47} {58..64} )
+    echo -n ${sp[$[ $RANDOM % 23 ]]} | awk '{printf("%c",$1)}'
 }
 cletter() {
-        c=( {65..90} )
-        echo -n ${c[$[ $RANDOM % 26 ]]} | awk '{printf("%c",$1)}'
+    c=( {65..90} )
+    echo -n ${c[$[ $RANDOM % 26 ]]} | awk '{printf("%c",$1)}'
 }
 array=( integer smletter spchar cletter )
 while ((count<n))
 do
-        ${array[$[ $RANDOM % 4 ]]}
-        ((count++))
+    ${array[$[ $RANDOM % 4 ]]}
+    ((count++))
 done
 echo
